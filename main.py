8857d28ce6742020895541f6038e9c3ef52b2f4c
iput-yamaki <yamatasu620@gmail.com>
2024-07-16 13:39:35 +0900

    平均値の追加

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index():
   data_list = []
+  total_temperature = 0
+  total_humidity = 0
+  count = 0
 
   print(f"ACCESS HOST:{request.remote_addr}")
   
@@ -21,13 +24,25 @@ def index():
       for row in reader:
         print(f">Row: {row}")
         data_list.append(row)
+        if len(row) >= 2:
+                    try:
+                        total_temperature += float(row[0])
+                        total_humidity += float(row[1])
+                        count += 1
+                    except ValueError:
+                        continue
   except FileNotFoundError:
     data_list = [["-", "-"]]
+  avg_temperature = total_temperature / count if count > 0 else 0
+  avg_humidity = total_humidity / count if count > 0 else 0
+
 
   return render_template('main.html',
                          title="DevPro3",
                          name="User",
                          data_list=data_list,
+                         avg_temperature=avg_temperature,
+                         avg_humidity=avg_humidity,
                          max_value=len(data_list))
 
 if __name__ == "__main__":
